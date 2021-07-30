247c7555072a07e7f7d6344059ca052ff44baea4
rathishvarathan <86611355+rathishvarathan@users.noreply.github.com>
2021-07-30 04:24:06 -0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,146 +1,171 @@
+import 'package:flop/privacy_policy.dart';
+import 'package:flop/send_feedback.dart';
+import 'package:flop/settings.dart';
 import 'package:flutter/material.dart';
-import 'package:duck/transit.dart';
-void main() {
-  runApp(MaterialApp(
-  home: HomePage(),
-  ));
-}
-class HomePage extends StatefulWidget {
 
+import 'contacts.dart';
+import 'dashboard.dart';
+import 'events.dart';
+import 'my_drawer_header.dart';
+import 'notes.dart';
+import 'notifications.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  var currentPage = DrawerSections.dashboard;
+
   @override
   Widget build(BuildContext context) {
+    var container;
+    if (currentPage == DrawerSections.dashboard) {
+      container = DashboardPage();
+    } else if (currentPage == DrawerSections.contacts) {
+      container = ContactsPage();
+    } else if (currentPage == DrawerSections.events) {
+      container = EventsPage();
+    } else if (currentPage == DrawerSections.notes) {
+      container = NotesPage();
+    } else if (currentPage == DrawerSections.settings) {
+      container = SettingsPage();
+    } else if (currentPage == DrawerSections.notifications) {
+      container = NotificationsPage();
+    } else if (currentPage == DrawerSections.privacy_policy) {
+      container = PrivacyPolicyPage();
+    } else if (currentPage == DrawerSections.send_feedback) {
+      container = SendFeedbackPage();
+    }
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Color(0xff21254A),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            height: 200,
-            child: Stack(
-              children:<Widget>[
-              Positioned(
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit:BoxFit.cover,
-                      image: AssetImage("assetName"),
-                ),
-              )
-             )
-            )
-           ],
+      appBar: AppBar(
+        backgroundColor: Colors.green[700],
+        title: Text("RATHISH"),
+      ),
+      body: container,
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                MyHeaderDrawer(),
+                MyDrawerList(),
+              ],
+            ),
           ),
-         ),
-           SizedBox(
-             height: 20,
-             width: 40,
-           ),
-         Padding(
-           padding: EdgeInsets.symmetric(horizontal: 20.0),
-           child: Column(
-             crossAxisAlignment: CrossAxisAlignment.start,
-             children: <Widget>[
-               Text("hello everyone,\nwelcome back",
-               style:TextStyle(
-                 fontSize:30,
-                 color: Colors.white,
-                 fontWeight: FontWeight.bold, 
-      
-               ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                padding: EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.transparent,
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: Colors.grey,
-                          )
-                        )
-                      ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Username",
-                          hintStyle: TextStyle(color: Colors.grey),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                          color: Colors.grey,
-                        ))
-                      ),
-                      child: TextField(
-                        decoration:InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Password",
-                          hintStyle: TextStyle(color: Colors.grey),
-                        ) ,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Center(
-                child: Text(
-                  "forgot password",
-                  style:TextStyle(
-                    color: Colors.pink[200],
-                  )
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 50,
-                margin:EdgeInsets.symmetric(horizontal: 60),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Color.fromRGBO(49,39,79,1),
-               ),
-               child:Center(
-                 child: Text(
-                   "login",
-                   style: TextStyle(color: Colors.white),
-                   ),
-               )
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Center(
-                child:Text("create account",style: TextStyle(color: Colors.pink[200]),
-                )
-              )
-             ]
-           ),
-         )
+        ),
+      ),
+    );
+  }
+
+  Widget MyDrawerList() {
+    return Container(
+      padding: EdgeInsets.only(
+        top: 15,
+      ),
+      child: Column(
+        // shows the list of menu drawer
+        children: [
+          menuItem(1, "Dashboard", Icons.dashboard_outlined,
+              currentPage == DrawerSections.dashboard ? true : false),
+          menuItem(2, "Contacts", Icons.people_alt_outlined,
+              currentPage == DrawerSections.contacts ? true : false),
+          menuItem(3, "Events", Icons.event,
+              currentPage == DrawerSections.events ? true : false),
+          menuItem(4, "Notes", Icons.notes,
+              currentPage == DrawerSections.notes ? true : false),
+          Divider(),
+          menuItem(5, "Settings", Icons.settings_outlined,
+              currentPage == DrawerSections.settings ? true : false),
+          menuItem(6, "Notifications", Icons.notifications_outlined,
+              currentPage == DrawerSections.notifications ? true : false),
+          Divider(),
+          menuItem(7, "Privacy policy", Icons.privacy_tip_outlined,
+              currentPage == DrawerSections.privacy_policy ? true : false),
+          menuItem(8, "Send feedback", Icons.feedback_outlined,
+              currentPage == DrawerSections.send_feedback ? true : false),
         ],
       ),
     );
   }
+
+  Widget menuItem(int id, String title, IconData icon, bool selected) {
+    return Material(
+      color: selected ? Colors.grey[300] : Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          Navigator.pop(context);
+          setState(() {
+            if (id == 1) {
+              currentPage = DrawerSections.dashboard;
+            } else if (id == 2) {
+              currentPage = DrawerSections.contacts;
+            } else if (id == 3) {
+              currentPage = DrawerSections.events;
+            } else if (id == 4) {
+              currentPage = DrawerSections.notes;
+            } else if (id == 5) {
+              currentPage = DrawerSections.settings;
+            } else if (id == 6) {
+              currentPage = DrawerSections.notifications;
+            } else if (id == 7) {
+              currentPage = DrawerSections.privacy_policy;
+            } else if (id == 8) {
+              currentPage = DrawerSections.send_feedback;
+            }
+          });
+        },
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Icon(
+                  icon,
+                  size: 20,
+                  color: Colors.black,
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+enum DrawerSections {
+  dashboard,
+  contacts,
+  events,
+  notes,
+  settings,
+  notifications,
+  privacy_policy,
+  send_feedback,
 }
